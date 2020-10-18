f2bdb3472725b99f74862c2ea3c7f7a6e7fa5d50
Blacky <hamidtessilimi@gmail.com>
2020-10-18 15:11:55 +0100

    update phpday08

--- C-DEV-111/phpday08/ex_09/inscription.php
+++ C-DEV-111/phpday08/ex_09/inscription.php
@@ -10,7 +10,7 @@
             $nameErr = $emailErr = $passwordErr = '';
             $name = $email = $password = $password_confirmation = '';
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                if (empty($_POST['name']))
+                if (empty($_POST['name']) || (strlen($_POST['name']) < 3 || strlen($_POST['name']) > 10))
                     $nameErr = "Invalid name";
                 else
                     $name = $_POST['name'];
@@ -20,26 +20,36 @@
                 else
                     $email = $_POST['email'];
 
-                if (empty($_POST['password']) || empty($_POST['password_confirmation']) || $_POST['password'] != $_POST['password_confirmation'])
+                if (empty($_POST['password']) || (strlen($_POST['password']) < 3 || strlen($_POST['password']) > 10) || $_POST['password'] != $_POST['password_confirmation'])
                     $passwordErr = "Invalid password or password confirmation";
                 else
-                    $password = password_hash($_POST['password']);
+                    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                if (!$nameErr && !$emailErr && !$passwordErr) {
+                    $user_created = array(
+                        "created_at" => date('Y-m-d H:i:s'),
+                        "password" => $password
+                    );
+                    $info = json_encode($user_created);
+                    file_put_contents("info.json", $info);
+                    $user = "User created";
+                }
             }
         ?>
 
-        <span class="error"><?php echo $nameErr; ?></span>
-        <span class="error"><?php echo $emailErr; ?></span>
-        <span class="error"><?php echo $passwordErr; ?></span>
+        <span class="error"><?php if ($nameErr) echo $nameErr."<br>"; ?></span>
+        <span class="error"><?php if ($emailErr) echo $emailErr."<br>"; ?></span>
+        <span class="error"><?php if ($passwordErr) echo $passwordErr."<br><br>"; ?></span>
+        <span class="error"><?php if ($user) echo $user."<br><br>"; ?></span>
 
         <form action="inscription.php" method="POST">
             <label for="name">Enter your name:</label><br>
-                <input type="text" id="name" name="name" value="name"><br>
+                <input type="text" id="name" name="name" placeholder="name"><br>
             <label for="email">Enter your mail:</label><br>
-                <input type="text" id="email" name="email" value="email"><br>
+                <input type="text" id="email" name="email" placeholder="email"><br>
             <label for="password">Votre mot de passe :</label><br>
-                <input type="password" name="password" id="password" value="password"/><br>
+                <input type="password" name="password" id="password" placeholder="password"/><br>
             <label for="password_confirmation">Votre mot de passe :</label><br>
-                <input type="password" name="password_confirmation" id="password" value="password_confirmation"/><br>
+                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="password_confirmation"/><br>
             <input type="submit" value="Submit"><br>
         </form>
     </body>
